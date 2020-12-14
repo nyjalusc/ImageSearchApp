be96e24902832885c2d56786f924f1830cbae017
Nyjal Augustine <nyjal@box.com>
2020-12-13 17:42:01 -0800

    To show Loading state for infinite scrolling - Implemented new view layout - And a new UnsplashPhotoLoadStateAdapter : LoadStateViewHolder - Instead of creating Interface to set click listener, I am passing onClickListener as a Fuction Type arg to the adapter

---
+++ app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/UnsplashPhotoLoadStateAdapter.kt
@@ -0,0 +1,49 @@
+package com.codinginflow.imagesearchapp.ui.gallery
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.paging.LoadState
+import androidx.paging.LoadStateAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.codinginflow.imagesearchapp.databinding.UnsplashPhotoLoadStateFooterBinding
+
+class UnsplashPhotoLoadStateAdapter(private val retry: () -> Unit) :
+    LoadStateAdapter<UnsplashPhotoLoadStateAdapter.LoadStateViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, loadState: LoadState): LoadStateViewHolder {
+        val binding = UnsplashPhotoLoadStateFooterBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return LoadStateViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: LoadStateViewHolder, loadState: LoadState) {
+        holder.bind(loadState)
+    }
+
+    /**
+     * Converted it to a inner class so that we can access retry function type property from the
+     * outer class otherwise we'll need to pass it through constructor.
+     */
+    inner class LoadStateViewHolder(
+        private val binding: UnsplashPhotoLoadStateFooterBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        // We only need to register the OnClickListener once so it is better to do it in the
+        // init block
+        init {
+            binding.buttonRetry.setOnClickListener { retry.invoke() }
+        }
+
+        fun bind(loadState: LoadState) {
+            binding.apply {
+                progressBar.isVisible = loadState is LoadState.Loading
+                buttonRetry.isVisible = loadState !is LoadState.Loading
+                textViewError.isVisible = loadState !is LoadState.Loading
+            }
+        }
+    }
+}
