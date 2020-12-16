8deffb157eaaed8997aae8c2484815ecfb9d8a08
Nyjal Augustine <nyjal@box.com>
2020-12-15 22:39:33 -0800

    Add ActionBar and link it with Navigation Controller - Handle navigate up

--- app/src/main/java/com/codinginflow/imagesearchapp/MainActivity.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/MainActivity.kt
@@ -2,7 +2,14 @@ package com.codinginflow.imagesearchapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.hilt.android.AndroidEntryPoint
+import java.security.AccessController
 
 /**
  * @AndroidEntryPoint annotation is used for performing DI in Android components such as
@@ -10,8 +17,27 @@ import dagger.hilt.android.AndroidEntryPoint
  */
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // findNavController() would work in other fragments and activities but not if the layout
+        // contains FragmentContainerView, that is the case here. Getting ref to NavController is
+        // a little different in that case. First find the Fragment attached to FragmentContainerView
+        // and cast it to NavHostFragment. Use this fragment to access NavController
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_main) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    // If the navController.navigateUp() returns false that means the intended navigation is outside the graph.
+    // If false is returned we let the system handle it.
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
